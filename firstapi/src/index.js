2a04213eec76a9d9dfd5625083bd4ddd79ab11e1
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-14 19:35:38 -0300

    1.2 - Criando a primeira rota da API

--- firstapi/src/index.js
+++ firstapi/src/index.js
@@ -1,8 +1,17 @@
 const http = require('http')
 
+const users = require('./mocks/users')
+
 const server = http.createServer((req, res) => {
+  console.log(`Request method: ${req.method} | Endpoint: ${req.url}`)
+
+  if (req.url === '/users' && req.method === 'GET') {
     res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('<h1>Hello world</h1>')
+    res.end(JSON.stringify(users))
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' })
+    res.end(`Cannot ${req.method} ${req.url}`)
+  }
 })
 
 server.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
