2d9384cd7f5890f32fcc92a5f55d59ebed8553d5
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-15 11:06:46 -0300

    2.0 - Criando um servidor http com Express

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(200, 'Hello!')
 })
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
