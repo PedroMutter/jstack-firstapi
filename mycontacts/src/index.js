25bd2fe71611342c114bc20137471311ce805915
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-16 11:54:38 -0300

    2.15 - Capturando e tratando erros

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+require('express-async-errors')
 
 const routes = require ('./routes')
 
@@ -6,5 +7,9 @@ const app = express()
 
 app.use(express.json())
 app.use(routes)
+app.use((err, req, res, next) => {
+  console.log(err)
+  res.sendStatus(500)
+})
 
 app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'))
