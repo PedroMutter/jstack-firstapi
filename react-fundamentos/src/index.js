9140ace130b629ca931ca9329ea8ce186b0b4797
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-17 12:45:05 -0300

    3.2 - Gerando o Bundle com o WebPack

--- react-fundamentos/src/index.js
+++ react-fundamentos/src/index.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { ReactDOM } from "react-dom"
+import ReactDOM from "react-dom"
 
 ReactDOM.render(
   <h1 id="title">
-    <span>Olá Mundo!</span>
+    <span>Hello World!!!!!!</span>
   </h1>,
   document.getElementById('root')
 )
