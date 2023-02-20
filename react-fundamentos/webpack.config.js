adf0af3e1d52933f2935d5f0a18568b1c3a74707
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-20 14:34:56 -0300

    3.19 - SASS e SASS Modules

--- react-fundamentos/webpack.config.js
+++ react-fundamentos/webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
         use: 'babel-loader'
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         exclude: /node_modules/,
         use: [
           'style-loader',
@@ -31,7 +31,8 @@ module.exports = {
             options: {
               modules: true
             }
-          }
+          },
+          'sass-loader'
         ]
       }
     ]
