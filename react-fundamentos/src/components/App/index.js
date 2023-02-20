d41258db6c66b2a139a8917dd84df367c7c1e2b6
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-20 16:04:41 -0300

    3.21 - Introdução ao Styled Components

--- react-fundamentos/src/components/App/index.js
+++ react-fundamentos/src/components/App/index.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react'
 
-import Post from './Post'
-import Header from './Header'
-import { ThemeProvider } from './ThemeContext'
+import Post from '../Post'
+import Header from '../Header'
+import { ThemeProvider } from '../../context/ThemeContext'
 
 import styles from './App.scss'
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false, removed: true },
+    { id: Math.random(), title: 'Title#01', subtitle: 'Sub#01', likes: 20, read: false, removed: false },
     { id: Math.random(), title: 'Title#02', subtitle: 'Sub#02', likes: 10, read: true, removed: false },
     { id: Math.random(), title: 'Title#03', subtitle: 'Sub#03', likes: 50, read: false, removed: false },
     { id: Math.random(), title: 'Title#04', subtitle: 'Sub#03', likes: 50, read: true, removed: false }
