8ff7b4b2447a0498df77b7eba0039adcc0e08f15
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-27 16:38:23 -0300

    4.8 - Consumindo a Context API

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -1,34 +1,25 @@
 import React from 'react'
-import { ThemeProvider } from 'styled-components'
+import { ThemeProvider as StyledThemeProvider } from 'styled-components'
 
 import GlobalStyle from './styles/global'
 import Layout from './components/Layout'
+import { ThemeProvider, ThemeContext } from './contexts/ThemeContext'
 
 import themes from './styles/themes'
 
 // Class Component
 class App extends React.Component {
-  state = {
-    theme: 'dark',
-    oiTudoBem: true
-  }
-
-  handleToggleTheme = () => {
-    this.setState(prevState => ({
-      theme: prevState.theme === 'dark' ? 'light' : 'dark'
-    }))
-  }
-
   render() {
-    const { theme } = this.state
-
     return (
-      <ThemeProvider theme={themes[theme] || themes.dark}>
-        <GlobalStyle />
-        <Layout
-          onToggleTheme={this.handleToggleTheme}
-          selectedTheme={theme}
-        />
+      <ThemeProvider>
+        <ThemeContext.Consumer>
+          {({ theme }) => (
+            <StyledThemeProvider theme={themes[theme] || themes.dark}>
+              <GlobalStyle />
+              <Layout />
+          </StyledThemeProvider>
+          )}
+        </ThemeContext.Consumer>
       </ThemeProvider>
     )
   }
