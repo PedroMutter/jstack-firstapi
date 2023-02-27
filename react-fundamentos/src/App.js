0009c5c45a7c9be9a5437c7887f42fc32432a9e7
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-27 16:46:30 -0300

    4.9 - componentDidMount

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -7,11 +7,25 @@ import { ThemeProvider, ThemeContext } from './contexts/ThemeContext'
 
 import themes from './styles/themes'
 
+// useEffect(() => {})      ---> inside render()
+// useEffect(() => {}, [])  ---> componentDidMount()
+
 // Class Component
 class App extends React.Component {
+  state = {
+    changed: false
+  }
+
+  componentDidMount() {
+    console.log('compoenentDidMout executed')
+  }
+
   render() {
+    console.log('rendered')
+
     return (
       <ThemeProvider>
+        <button onClick={() => this.setState({ changed: true })}>Change state</button>
         <ThemeContext.Consumer>
           {({ theme }) => (
             <StyledThemeProvider theme={themes[theme] || themes.dark}>
