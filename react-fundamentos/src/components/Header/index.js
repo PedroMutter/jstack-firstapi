ee481df4ac86c895975fde5b4a4703349fdcbde6
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-28 16:25:58 -0300

    4.17 - HOC para monitorar alterações na Context API

--- react-fundamentos/src/components/Header/index.js
+++ react-fundamentos/src/components/Header/index.js
@@ -3,23 +3,40 @@ import React, { Component } from 'react'
 import { ThemeContext } from '../../contexts/ThemeContext' 
 import { Container } from './styles'
 
+function HOC(ComponenteHeader) {
+  return class Component extends React.Component {
+    render() {
+      return (
+        <ThemeContext.Consumer>
+          {(value) => (
+            <ComponenteHeader {...value} />
+          )}
+        </ThemeContext.Consumer>
+      )
+    }
+  }
+}
+
 // Class Component
-export default class Header extends Component {
+class Header extends Component {
+  componentDidUpdate(prevProps, prevState) {
+    if (this.props.theme !== prevProps.theme) {
+      console.log('tema mudou')
+    }
+  }
   render() {
     return(
-      <ThemeContext.Consumer>
-        {({ theme, handleToggleTheme }) => (
-          <Container>
-            <h1>JStack's Blog</h1>
-            <button
-              type="button"
-              onClick={handleToggleTheme}
-            >
-              {theme === 'dark' ? '🌞' : '🌚'}
-            </button>
-          </Container>
-        )}
-      </ThemeContext.Consumer>
+      <Container>
+        <h1>JStack's Blog</h1>
+        <button
+          type="button"
+          onClick={this.props.handleToggleTheme}
+        >
+          {this.props.theme === 'dark' ? '🌞' : '🌚'}
+        </button>
+      </Container>
     )
   }
 }
+
+export default HOC(Header)
