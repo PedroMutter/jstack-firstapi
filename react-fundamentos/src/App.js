e1f8a9e84dda8c300039635c4730cc9ab6c0ec69
Pedro Mutter <pedro.mutter1@gmail.com>
2023-02-28 16:09:34 -0300

    4.15 - Salvando o theme no localStorage

--- react-fundamentos/src/App.js
+++ react-fundamentos/src/App.js
@@ -7,55 +7,16 @@ import { ThemeProvider, ThemeContext } from './contexts/ThemeContext'
 
 import themes from './styles/themes'
 
-// useEffect(() => {})      ---> inside render()
-// useEffect(() => {}, [])  ---> componentDidMount()
-
 // Class Component
 class App extends React.Component {
-  state = {
-    changed: false
-  }
-
-  componentDidMount() {
-    console.log('compoenentDidMount executed')
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate', {
-      currentState: this.state,
-      prevState,
-      prevProps
-    })
-  }
-
-  componentDidCatch(error, info) {
-    console.log('componentDidCatch', { error, info })
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('shouldComponentUpdate', {
-      currentState: this.state,
-      nextState,
-      nextProps
-    })
-
-    return true
-  }
-
   render() {
-    console.log('rendered')
-
     return (
       <ThemeProvider>
         <ThemeContext.Consumer>
-          {({ theme, handleToggleTheme }) => (
+          {({ theme }) => (
             <StyledThemeProvider theme={themes[theme] || themes.dark}>
               <GlobalStyle />
-              <button onClick={handleToggleTheme}>
-                Change state
-              </button>
-              {theme === 'dark' && <Layout />}
-              <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+              <Layout />
           </StyledThemeProvider>
           )}
         </ThemeContext.Consumer>
--- react-fundamentos/src/components/Layout/index.js
+++ react-fundamentos/src/components/Layout/index.js
@@ -5,20 +5,6 @@ import PostsList from '../PostsList'
 import Footer from '../Footer'
 
 export default class Layout extends React.Component {
-  componentDidMount() {
-    console.log('componente montou')
-    document.addEventListener('scroll', this.handleScroll)
-  }
-
-  componentWillUnmount() {
-    console.log('componente vai desmontar...')
-    document.removeEventListener('scroll', this.handleScroll)
-  }
-
-  handleScroll = () => {
-    console.log('scrolled...')
-  }
-
   render() {
     return (
       <>
