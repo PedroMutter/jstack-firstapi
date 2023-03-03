d800595af80053ffe1d0ad442c680189b5f12274
Pedro Mutter <pedro.mutter1@gmail.com>
2023-03-03 03:56:52 -0300

    6.10 Criando os componentes Input e Select

--- mycontacts/fe/src/pages/NewContact/index.js
+++ mycontacts/fe/src/pages/NewContact/index.js
@@ -1,9 +1,21 @@
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 export default function NewContact() {
   return (
-    <PageHeader
-      title="Novo Contato"
-    />
+    <>
+      <PageHeader
+        title="Novo Contato"
+      />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">instagram</option>
+        <option value="123">instagram</option>
+        <option value="123">instagram</option>
+        <option value="123">instagram</option>
+      </Select>
+    </>
   );
 }
